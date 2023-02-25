4d9bf1dbfe665b07306ba903057698b767ac4004
Daryl Wattima <darwattima@gmail.com>
2023-02-25 13:38:53 +0300

    Completed assignment

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,19 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show 
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
+
+# class CreateCharacters < ActiveRecord::Migration[6.1]
+#   def change
+#     # Your code here
+#     create_table :characters do |t|
+#       t.string :name 
+#       t.integer :actor_id
+#       t.integer :show_id
+#     end
+#   end
+# end
